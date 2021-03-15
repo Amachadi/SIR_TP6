97fd4d78a3eb63649717068c9eed943631cc51be
anwar.machadi@gmail.com <anwar@100>
2021-03-15 12:06:27 +0100

    Q1-Q5

--- interaction.js
+++ interaction.js
@@ -3,10 +3,61 @@
 // L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
 	// Définir ici les attributs de la 'classe'
-
+  // la position initiale
+  this.x1=0;
+  this.y1=0;
+  // la position finale
+  this.x2=0;
+  this.y2=0;
+  this.press=false;
 	// Developper les 3 fonctions gérant les événements
 
+  this.pression = function(event)
+  {
+
+    this.press=true;
+
+    console.log("press");
+
+    interactor.onInteractionStart(this);
+
+
+  }.bind(this); // lies la method a la classe Dnd
+
+  this.deplacer= function(event)
+  {
+    if(this.press)
+    {
+      var res=getMousePosition(canvas,event);
+      this.x2=res.x;
+      this.y2=res.y;
+
+      interactor.onInteractionUpdate(this);
+
+      console.log(res);
+    }
+
+  }.bind(this); // lies la method a la classe Dnd
+
+  this.relacher = function(event)
+  {
+    if(this.press)
+    {
+      var res=getMousePosition(canvas,event);
+      this.x2=res.x;
+      this.y2=res.y;
+
+      interactor.onInteractionEnd(this);
+
+      console.log(res);
+    }
+
+
+  }.bind(this); // lies la method a la classe Dnd
 	// Associer les fonctions précédentes aux évènements du canvas.
+  canvas.addEventListener('mousedown', this.pression, false);
+  canvas.addEventListener('mousemove', this.deplacer, false);
+  canvas.addEventListener('mouseup', this.relacher, false);
 };
 
 
